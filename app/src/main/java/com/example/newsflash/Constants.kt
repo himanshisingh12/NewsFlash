effd7cdf0a52e277d257499b725bed49560ff9b1
himanshisingh <singh.himanshi77@gmail.com>
2021-03-01 10:36:10 +0530

    remove api key

--- app/src/main/java/com/example/newsflash/Constants.kt
+++ app/src/main/java/com/example/newsflash/Constants.kt
@@ -6,7 +6,7 @@ object Constants {
     /* Urls*/
     const val NEWS_LIST_BASE_URL = "https://newsapi.org/v2/"
     const val STATS_BASE_URL = "https://cn-news-info-api.herokuapp.com/"
-    const val API_KEY = "8735d49f66ad423dbcf7cb0a3838285d"
+    const val API_KEY = ""
     const val KEY_ID = "ARTICLE ID"
     const val KEY_URL = "ARTICLE URL"
 
